0d7881240b213eb3254893c74c5f94a8d3c45451
dage <dgeiss438@gmail.com>
2019-09-19 11:28:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppBar from './components/AppBar/AppBar';
-import Main from './components/Main/Main';
-import About from './components/About/About';
-import Study from './components/Study/Study';
-import Contact from './components/Contact/Contact';
+// import Main from './components/Main/Main';
+// import About from './components/About/About';
+// import Study from './components/Study/Study';
+// import Contact from './components/Contact/Contact';
 import './App.scss';
 
 function App() {
@@ -12,12 +12,12 @@ function App() {
     <React.Fragment>
       <Router>
         <AppBar />
-        <Switch>
+    {/*<Switch>
           <Route path="/" exact component={Main} />
           <Route path="/about" component={About} />
           <Route path="/study" component={Study} />
           <Route path="/contact" component={Contact} />
-        </Switch>
+        </Switch>*/}
       </Router>
     </React.Fragment>
   );
